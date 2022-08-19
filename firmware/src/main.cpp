5317b17953491fee89e72b2b92f49893e0ff42b0
Norbert Szulc <norbert@not7cd.net>
2022-08-19 11:05:23 +0200

    Get data from spaceapi

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,11 +1,48 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+
+const char *resource = "https://status.hsp.sh/api/now";
+const char* exthost = "https://status.hsp.sh";
+HTTPClient http;
+WiFiClientSecure client;
 
 const uint8_t RED_PIN = D1;
 const uint8_t GREEN_PIN = D2;
 const uint8_t BLUE_PIN = D3;
 static int brightness;
 int fadeAmount = 5;
+
 static unsigned char pos = 0;
+
+StaticJsonDocument<1024> json;
+static bool isOpen = true;
+unsigned long lastRequest = 0;
+
+bool getSpaceStatus()
+{
+  String payload = "";
+  http.begin(client, resource);
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK)    
+  {
+    payload = http.getString();
+
+    http.end(); //Free the resources
+    DeserializationError error = deserializeJson(json, payload);
+    if (error != DeserializationError::Ok)
+    {
+      return false;
+    }
+    bool result = json["state"]["open"];
+    return result;
+  }
+  http.end(); // Close connection
+  return false;
+}
 
 void setColor(int r, int g, int b)
 {
@@ -36,11 +73,38 @@ void Wheel(unsigned char WheelPos)
 
 void setup()
 {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  client.setInsecure(); //the magic line, use with caution
+  client.connect(exthost, 433);
+  isOpen = false;
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    pos += 10;
+    Wheel(pos);
+    delay(10);
+  }
+  isOpen = getSpaceStatus();
 }
 
 void loop()
 {
+  if (WiFi.status() == WL_CONNECTED && millis() - lastRequest >= 1000 * 60)
+  {
+    isOpen = getSpaceStatus();
+    lastRequest = millis();
+  }
   pos++;
-  Wheel(pos);
-  delay(300); //delaying the speed of fading
+  if (isOpen)
+  {
+    Wheel(pos);
+  }
+  else
+  {
+    setColor(0, 0, 0);
+  }
+
+  delay(300); // delaying the speed of fading
 }
